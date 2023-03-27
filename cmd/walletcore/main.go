0f39104ff4b1438efd3ff4c4ce191644ee406fe1
Raphael Berger <raphaelmb@gmail.com>
2023-03-27 01:00:21 -0300

    add kafka integration

--- cmd/walletcore/main.go
+++ cmd/walletcore/main.go
@@ -3,29 +3,40 @@ package main
 import (
 	"context"
 	"database/sql"
+	"fmt"
 
+	ckafka "github.com/confluentinc/confluent-kafka-go/kafka"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/raphaelmb/fullcycle-walletcore/internal/database"
 	"github.com/raphaelmb/fullcycle-walletcore/internal/event"
+	"github.com/raphaelmb/fullcycle-walletcore/internal/event/handler"
 	createaccount "github.com/raphaelmb/fullcycle-walletcore/internal/usecase/create_account"
 	createclient "github.com/raphaelmb/fullcycle-walletcore/internal/usecase/create_client"
 	createtransaction "github.com/raphaelmb/fullcycle-walletcore/internal/usecase/create_transaction"
 	"github.com/raphaelmb/fullcycle-walletcore/internal/web"
 	"github.com/raphaelmb/fullcycle-walletcore/internal/web/webserver"
 	"github.com/raphaelmb/fullcycle-walletcore/pkg/events"
+	"github.com/raphaelmb/fullcycle-walletcore/pkg/kafka"
 	"github.com/raphaelmb/fullcycle-walletcore/pkg/uow"
 )
 
 func main() {
-	db, err := sql.Open("mysql", "root:root@tcp(localhost:3306)/wallet?charset=utf8&parseTime=True&loc=Local")
+	db, err := sql.Open("mysql", "root:root@tcp(mysql:3306)/wallet?charset=utf8&parseTime=True&loc=Local")
 	if err != nil {
 		panic(err)
 	}
 	defer db.Close()
 
+	configMap := ckafka.ConfigMap{
+		"bootstrap.servers": "kafka:29092",
+		"group.id":          "wallet",
+	}
+
+	kafkaProducer := kafka.NewKafkaProducer(&configMap)
+
 	eventDispatcher := events.NewEventDispatcher()
+	eventDispatcher.Register("TransactionCreated", handler.NewTransactionCreatedKafkaHandler(kafkaProducer))
 	transactionCreatedEvent := event.NewTransactionCreated()
-	// eventDispatcher.Register("TransactionCreated", handler)
 
 	clientDb := database.NewClientDB(db)
 	accountDb := database.NewAccountDB(db)
@@ -45,7 +56,7 @@ func main() {
 	createAccountUseCase := createaccount.NewCreateAccountUseCase(accountDb, clientDb)
 	createTransactionUseCase := createtransaction.NewCreateTransactionUseCase(uow, eventDispatcher, transactionCreatedEvent)
 
-	webserver := webserver.NewWebServer(":3000")
+	webserver := webserver.NewWebServer(":8080")
 
 	clientHandler := web.NewWebClientHandler(*createClientUseCase)
 	accountHandler := web.NewWebAccountHandler(*createAccountUseCase)
@@ -55,5 +66,6 @@ func main() {
 	webserver.AddHandler("/accounts", accountHandler.CreateAccount)
 	webserver.AddHandler("/transactions", transactionHandler.CreateTransaction)
 
+	fmt.Println("Server is running")
 	webserver.Start()
 }
